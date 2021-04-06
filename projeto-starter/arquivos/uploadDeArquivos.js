6a0e33bd3a706420df98bf81dc94a009b85b1755
AlessandroMaia <ale.maia.alm@gmail.com>
2021-04-06 00:45:57 -0300

    Streaming de dados

---
+++ projeto-starter/arquivos/uploadDeArquivos.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+
+fs.createReadStream('./assets/salsicha.jpg')
+    .pipe(fs.createWriteStream('./assets/salsicha-stream.jpg'))
+    .on('finish', () => {
+        console.log("Imagem foi escrita com sucesso!");
+    });
