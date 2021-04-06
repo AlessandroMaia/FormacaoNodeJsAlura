4fd98724419df4320d20ec1c9619360e860c7259
AlessandroMaia <ale.maia.alm@gmail.com>
2021-04-06 09:41:43 -0300

    Manipulação de arquivos

--- projeto-starter/infraestrutura/tabelas.js
+++ projeto-starter/infraestrutura/tabelas.js
@@ -2,10 +2,11 @@ class Tabelas {
     init(conexao) {
         this.conexao = conexao;
         this.criarAtendimentos();
+        this.criarPets();
     }
 
-    criarAtendimentos(){
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT,' 
+    criarAtendimentos() {
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT,'
             + 'cliente varchar(50) NOT NULL,'
             + 'pet varchar(20) NOT NULL,'
             + 'servico varchar(50) NOT NULL,'
@@ -13,11 +14,27 @@ class Tabelas {
             + 'dataCriacao datetime NOT NULL,'
             + 'status varchar(20) NOT NULL,'
             + 'observacoes text, PRIMARY KEY(id))';
+
         this.conexao.query(sql, (erro) => {
-            if(erro){
+            if (erro) {
                 console.log(erro);
-            }else{
+            } else {
                 console.log('Tabela atendimentos criada com sucesso')
+            }
+        });
+    }
+
+    criarPets() {
+        const sql = ' CREATE TABLE IF NOT EXISTS Pets (id int NOT NULL AUTO_INCREMENT,'
+            + 'nome varchar(50),'
+            + 'imagem varchar(200),'
+            + 'PRIMARY KEY (id))';
+
+        this.conexao.query(sql, (erro) => {
+            if (erro) {
+                console.log(erro);
+            } else {
+                console.log('Tabela Pets criada com sucesso')
             }
         });
     }
