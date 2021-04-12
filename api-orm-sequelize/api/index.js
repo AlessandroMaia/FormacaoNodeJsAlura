429edd53d42ea626ef16c12d134d8670d7386e2f
AlessandroMaia <ale.maia.alm@gmail.com>
2021-04-12 13:41:32 -0300

    Controladores e rotas

--- api-orm-sequelize/api/index.js
+++ api-orm-sequelize/api/index.js
@@ -1,16 +1,10 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const routes = require('./routes')
 
 const app = express()
-
-app.use(bodyParser.json())
-
 const port = 3000
 
-app.get('/', (requisicao, resposta) => resposta
-    .status(200)
-    .send({
-        messagem: 'Boas vindas a API'
-    }))
+routes(app)
 
 app.listen(port, () => console.log(`O servidor está rodando na porta ${port}`))
